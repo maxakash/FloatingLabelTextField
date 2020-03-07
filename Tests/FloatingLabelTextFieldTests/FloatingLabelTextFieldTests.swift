c171433e1bc6c91548d0ad8c4d11e4084c04fcbc
maxakash <akashchaudharymax@gmail.com>
2020-03-07 20:07:03 +0530

    initial commit

--- Tests/FloatingLabelTextFieldTests/FloatingLabelTextFieldTests.swift
+++ Tests/FloatingLabelTextFieldTests/FloatingLabelTextFieldTests.swift
@@ -6,10 +6,8 @@ final class FloatingLabelTextFieldTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(FloatingLabelTextField(isSecure: false).text, "Hello, World!")
+      //  XCTAssertEqual( "Hello, World!")
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+   
 }
